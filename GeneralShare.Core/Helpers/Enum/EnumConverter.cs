a69d14885805ab790eff44a983a916c5108f647d
Michal Piatkowski <michal@kampinas.se>
2019-04-02 17:43:49 +0200

    Added enum to long to EnumConverter

--- GeneralShare.Core/Helpers/Enum/EnumConverter.cs
+++ GeneralShare.Core/Helpers/Enum/EnumConverter.cs
@@ -5,11 +5,12 @@ namespace GeneralShare
 {
     public static class EnumConverter<TEnum> where TEnum : Enum
     {
-        private static readonly Func<long, TEnum> _convert = GenerateConverter();
-        
+        private static readonly Func<long, TEnum> _convertTo = GenerateToConverter();
+        private static readonly Func<TEnum, long> _convertFrom = GenerateFromConverter();
+
         public static TEnum Convert(long value)
         {
-            return _convert.Invoke(value);
+            return _convertTo.Invoke(value);
         }
 
         public static TEnum Convert(ulong value)
@@ -17,11 +18,24 @@ namespace GeneralShare
             return Convert((long)value);
         }
 
-        private static Func<long, TEnum> GenerateConverter()
+        public static long Convert(TEnum value)
+        {
+            return _convertFrom.Invoke(value);
+        }
+
+        private static Func<long, TEnum> GenerateToConverter()
         {
             var parameter = Expression.Parameter(typeof(long));
             var conversion = Expression.Convert(parameter, typeof(TEnum));
             var method = Expression.Lambda<Func<long, TEnum>>(conversion, parameter);
+            return method.Compile();
+        }
+
+        private static Func<TEnum, long> GenerateFromConverter()
+        {
+            var parameter = Expression.Parameter(typeof(TEnum));
+            var conversion = Expression.Convert(parameter, typeof(long));
+            var method = Expression.Lambda<Func<TEnum, long>>(conversion, parameter);
             return method.Compile();
         }
     }
